f748bc48d1aeb7db5348e0cc5312a1d1cc4d98cd
Simon <sstur@me.com>
2023-04-06 16:47:42 -0700

    task 49 setup

---
+++ tests/task49.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('can navigate to login', async ({ page }) => {
+  await page.goto('https://mcy0dn.csb.app/');
+
+  // Task 49
+  // Write a test that will click the icon button in the upper left and assert
+  // that the browser has navigated to the login page.
+});
