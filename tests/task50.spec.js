c7040b4199c856341f444822049f51d75b0b5e7a
Simon <sstur@me.com>
2023-04-06 16:53:07 -0700

    task 50 setup

--- tests/task50.spec.js
+++ tests/task50.spec.js
@@ -8,4 +8,11 @@ test('can navigate to login', async ({ page }) => {
   await expect(page).toHaveURL('https://mcy0dn.csb.app/login');
 
   await expect(page.getByRole('heading')).toContainText('Login');
+
+  // Task 50
+  // Now fill out the login form and submit
+  // After successful login, you should be navigated back
+  // to the home page.
+  // Username: test-user
+  // Password: 123
 });
