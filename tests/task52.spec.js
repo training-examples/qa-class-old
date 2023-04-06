b3dcc4ba7e5b02c382b49528d4b9f06889c8c905
Simon <sstur@me.com>
2023-04-06 16:58:20 -0700

    task 52 solution

--- tests/task52.spec.js
+++ tests/task52.spec.js
@@ -3,8 +3,13 @@ import { test, expect } from '@playwright/test';
 test('can navigate to login', async ({ page }) => {
   await page.goto('https://demo.realworld.io/');
 
-  // Task 52
-  // Test the login flow using the username and password below:
-  // Username: test-user@sstur.com
-  // Password: asdfasdf
+  await page.getByRole('link', { name: 'Sign in' }).click();
+
+  await expect(page).toHaveURL('https://demo.realworld.io/#/login');
+
+  await page.getByPlaceholder('Email').fill('test-user@sstur.com');
+  await page.getByPlaceholder('Password').fill('asdfasdf');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+
+  await expect(page).toHaveURL('https://demo.realworld.io/#/');
 });
