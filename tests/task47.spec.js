5fd579ae3fcd10c5ed4cc34c592a489a2b800caa
Simon <sstur@me.com>
2023-04-06 16:16:04 -0700

    task setup

---
+++ tests/task47.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('displays movies', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Task 47
+  // Write a test to ensure a movie is displayed
+});
